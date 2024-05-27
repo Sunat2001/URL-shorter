32375d04ea970931e44f52acfb2f0576a96ed89e
Сунат Боев <boev_sunat@keepcode.org>
2024-05-27 11:32:03 +0500

    Finish main functionaluty

---
+++ internel/config/config.go
@@ -0,0 +1,39 @@
+package config
+
+import (
+	"github.com/ilyakaznacheev/cleanenv"
+	"log"
+	"os"
+	"time"
+)
+
+type Config struct {
+	Env         string `yaml:"env" env-default:"development"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HTTPServer  `yaml:"http_server"`
+}
+
+type HTTPServer struct {
+	Address     string        `yaml:"address" env-default:"0.0.0.0:8080"`
+	Timeout     time.Duration `yaml:"timeout" env-default:"5s"`
+	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
+}
+
+func MustLoad() *Config {
+	configPath := "C:\\Users\\Sunat\\Desktop\\Projects\\GO\\Url-shorter\\config\\local.yml"
+	//if configPath == "" {
+	//	log.Fatal("CONFIG_PATH is not set")
+	//}
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("Not found %v", configPath)
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatalf("cannot read config: %v", err)
+	}
+
+	return &cfg
+}
---
+++ internel/storage/sqlite/sqlite.go
@@ -0,0 +1,85 @@
+package sqlite
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	_ "modernc.org/sqlite"
+	"url-shortner/internel/storage"
+)
+
+type Storage struct {
+	db *sql.DB
+}
+
+func New(stargePath string) (*Storage, error) {
+	const op = "storage.sqlite.New"
+
+	db, err := sql.Open("sqlite", stargePath)
+	if err != nil {
+		return nil, fmt.Errorf("%s, %w", op, err)
+	}
+
+	stmt, err := db.Prepare(`
+	CREATE TABLE IF NOT EXISTS url(
+	    id INTEGER PRIMARY KEY,
+	    alias TEXT NOT NULL UNIQUE,
+	    url TEXT NOT NULL);
+	CREATE INDEX IF NOT EXISTS idx_alias ON url(alias)
+	`)
+	if err != nil {
+		return nil, fmt.Errorf("%s, %w", op, err)
+	}
+
+	_, err = stmt.Exec()
+	if err != nil {
+		return nil, fmt.Errorf("%s, %w", op, err)
+	}
+
+	return &Storage{db: db}, nil
+}
+
+func (s *Storage) SaveURL(urlToSave, alias string) (int64, error) {
+	const op = "storage.sqlite.SaveURL"
+
+	stmt, err := s.db.Prepare("INSERT INTO url(url, alias) VALUES(?, ?)")
+	if err != nil {
+		return 0, fmt.Errorf("%s, %w", op, err)
+	}
+
+	res, err := stmt.Exec(urlToSave, alias)
+	if err != nil {
+		return 0, fmt.Errorf("%s, %w", op, err)
+	}
+
+	id, err := res.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("%s, failed to get last insert id: %w", op, err)
+	}
+
+	return id, nil
+}
+
+func (s *Storage) GetURL(alias string) (string, error) {
+	const op = "storage.sqlite.GetURL"
+
+	stmt, err := s.db.Prepare("SELECT url FROM url WHERE alias = ?")
+	if err != nil {
+		return "", fmt.Errorf("%s, %w", op, err)
+	}
+
+	var url string
+	err = stmt.QueryRow(alias).Scan(&url)
+	if errors.Is(err, sql.ErrNoRows) {
+		return "", storage.ErrURLNotFound
+	}
+
+	return url, nil
+}
+
+func (s *Storage) CloseConnection() {
+	err := s.db.Close()
+	if err != nil {
+		panic(err)
+	}
+}
